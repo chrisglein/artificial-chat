9e2d4da7650ea14905c8d2b417715c1f84c05979
Chris Glein <26607885+chrisglein@users.noreply.github.com>
2023-03-06 17:21:11 -0800

    Move styles definition to Styles.tsx

--- Styles.tsx
+++ Styles.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 
 type StylesType = {
   appContent: any;
@@ -16,4 +17,70 @@ type StylesType = {
 }
 const StylesContext = React.createContext<StylesType>({});
 
-export { StylesContext };
+const CreateStyles = (isDarkMode: boolean) => {
+  return StyleSheet.create({
+    appContent: {
+      backgroundColor: isDarkMode ? 'black' : 'white',
+      justifyContent: 'space-between',
+      height: '100%',
+    },
+    popupBackground: {
+      backgroundColor: isDarkMode ? 'white' : 'black',
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      opacity: 0.3,
+    },
+    sectionContainer: {
+      marginHorizontal: 12,
+      paddingHorizontal: 24,
+      paddingVertical: 12,
+      borderRadius: 8,
+    },
+    humanSection: {
+      backgroundColor: isDarkMode ? '#333355' : 'lightblue',
+      marginRight: 64,
+    },
+    aiSection: {
+      backgroundColor: isDarkMode ? '#444444' : 'lightgray',
+      marginLeft: 64,
+    },
+    sectionTitle: {
+      fontSize: 12,
+      fontWeight: '600',
+    },
+    highlight: {
+      fontWeight: '700',
+    },
+    horizontalContainer: {
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      alignItems: 'center',
+      gap: 12,
+    },
+    dalleImage: {
+      width: 150,
+      height: 150,
+    },
+    inlineCard: {
+      borderColor: 'gray',
+      borderWidth: 2,
+      borderRadius: 8,
+      padding: 8,
+    },
+    feedbackDialog: {
+      backgroundColor: isDarkMode ? 'black' : 'white',
+      padding: 12,
+      borderRadius: 8,
+      minWidth: 300
+    },
+    codeBlockTitle: {
+      backgroundColor: isDarkMode ? 'white' : '#444',
+    },
+    codeBlockTitleText: {
+      color: isDarkMode ? 'black' : 'white',
+    },
+  });
+}
+
+export { StylesContext, CreateStyles };
